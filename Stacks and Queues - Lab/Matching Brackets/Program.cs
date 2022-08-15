8cab3f36ad440a7d3e908ca7030ea9e90ebf4294
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-15 23:41:41 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/Matching Brackets/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Stack<int> myStack = new Stack<int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char symbol = text[i];
+
+
+                if (symbol == '(')
+                {
+                    
+
+                    myStack.Push(i);
+                }
+
+                else if (symbol == ')')
+                {
+                    var result = myStack.Pop();
+
+                    string sub = text.Substring(result, i - result + 1);
+
+                    Console.WriteLine(sub);
+                }
+            }
+        }
+    }
+}
