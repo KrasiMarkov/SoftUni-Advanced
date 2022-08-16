4102d37a7b9b31c179e67750d5cbc758ddd73a93
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-16 23:00:31 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/Simple Calculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            Stack<string> myStack = new Stack<string>();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+
+                myStack.Push(input[i]);
+
+
+            }
+
+            while (myStack.Count > 1)
+            {
+
+                int operandOne = int.Parse(myStack.Pop());
+                string opr = myStack.Pop();
+                int operandTwo = int.Parse(myStack.Pop());
+
+                int sum = 0;
+
+                if (opr == "+")
+                {
+                    sum = operandOne + operandTwo;
+                }
+
+                else
+                {
+                    sum = operandOne - operandTwo;
+                }
+
+                myStack.Push(sum.ToString());
+            }
+
+            Console.WriteLine(myStack.Peek());
+
+
+        }
+    }
+}
