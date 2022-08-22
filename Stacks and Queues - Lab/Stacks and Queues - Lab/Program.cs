19503c6aae4201a6b26038247f6e8424cba8f239
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-22 23:07:32 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/Stacks and Queues - Lab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Stacks_and_Queues___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> myStack = new Stack<char>();
+
+            foreach (var item in input)
+            {
+                myStack.Push(item);
+            }
+
+            
+
+            while (myStack.Count > 0)
+            {
+                Console.Write(myStack.Pop());
+            }
+
+        }
+    }
+}
