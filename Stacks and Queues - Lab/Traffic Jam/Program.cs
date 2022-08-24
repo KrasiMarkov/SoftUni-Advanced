31b39866ac9fdb2c7edff0e223f19e16a2d8521a
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-24 23:28:13 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/Traffic Jam/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> cars = new Queue<string>();
+
+            int counter = 0;
+
+            while (true)
+            {
+                string car = Console.ReadLine();
+
+                if (car == "end")
+                {
+                    break;
+                }
+
+                else if (car == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+
+                        if (cars.Count == 0)
+                        {
+                            break;
+                        }
+
+                        string outCar = cars.Dequeue();
+
+                        counter++;
+
+                        Console.WriteLine($"{outCar} passed!");
+
+                       
+                    }
+                    continue;
+                }
+
+                cars.Enqueue(car);
+
+            }
+
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
