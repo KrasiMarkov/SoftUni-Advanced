965cb0e9db8f69de8727b87c819afb360b92ef69
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-24 23:27:45 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/Supermarket/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> myQueue = new Queue<string>();
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+
+                if (name == "End")
+                {
+                    break;
+                }
+
+                else if (name == "Paid")
+                {
+                    while (myQueue.Count > 0)
+                    {
+                        Console.WriteLine(myQueue.Dequeue());
+                    }
+
+                    continue;
+                }
+                myQueue.Enqueue(name);
+
+            }
+
+            Console.WriteLine($"{myQueue.Count} people remaining.");
+
+
+        }
+    }
+}
