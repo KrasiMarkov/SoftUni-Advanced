14e119c7be11ab1c5ffe379953aef492f569a96d
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-26 23:45:51 +0300

    Add files via upload

---
+++ Stacks and Queues - Exercise/Basic Queue Operations/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> myQueue = new Queue<int>();
+
+            int N = numbers[0];
+
+            int S = numbers[1];
+
+            int X = numbers[2];
+
+
+
+
+
+            for (int i = 0; i < N; i++)
+            {
+                int num = arr[i];
+
+               myQueue.Enqueue(num);
+
+            }
+
+            for (int i = 0; i < S; i++)
+            {
+                myQueue.Dequeue();
+
+
+            }
+
+
+            if (myQueue.Contains(X))
+            {
+                Console.WriteLine("true");
+            }
+            else if (myQueue.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine(myQueue.Min());
+            }
+        }
+    }
+}
