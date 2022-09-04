a39f3857d49a91b56deecee0d019e31e4db015a9
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-09-04 22:53:55 +0300

    Add files via upload

---
+++ Stacks and Queues - Exercise/Fashion Boutique/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int capacityOfRack = int.Parse(Console.ReadLine());
+
+            Stack<int> clothes = new Stack<int>(sequence);
+
+            int capacity = capacityOfRack;
+
+            int countRacks = 1;
+
+            while (clothes.Count > 0)
+            {
+               int current = clothes.Peek();
+
+
+                if (current <= capacity)
+                {
+                    int currentNumber = clothes.Pop();
+
+                    capacity -= currentNumber;
+                    
+                    
+                }
+
+                else
+                {
+                    countRacks++;
+
+                    capacity = capacityOfRack;
+
+
+                }
+
+               
+            }
+
+            Console.WriteLine(countRacks);
+
+        }
+    }
+}
