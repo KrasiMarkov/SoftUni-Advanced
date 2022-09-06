0a4567c039a36d73fc506ffad26bde026c102145
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-09-06 20:07:17 +0300

    Add files via upload

---
+++ Stacks and Queues - Exercise/Songs Queue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sequence = Console.ReadLine().Split(", ").ToArray();
+
+            Queue<string> songs = new Queue<string>(sequence);
+
+            while (songs.Count > 0)
+            {
+                string[] command = Console.ReadLine().Split(" ", 2);
+
+                if (command[0] == "Play")
+                {
+                    songs.Dequeue();
+                }
+
+                else if (command[0] == "Add")
+                {
+                    string song = command[1];
+
+                    if (!songs.Contains(song))
+                    {
+                        songs.Enqueue(song);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                }
+
+                else if (command[0] == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
