0c41bc24e36a440cea9983370fece6689e13ec50
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-09-08 22:17:54 +0300

    Add files via upload

---
+++ Lab_Multidimensional Arrays/Primary Diagonal/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+namespace Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currentRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                    matrix[row, col] = currentRow[col];
+
+
+                }
+
+            }
+
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                    sum += matrix[row, row];
+
+
+
+                    break;
+
+
+                }
+
+                
+
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
