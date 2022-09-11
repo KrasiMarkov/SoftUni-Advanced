846f0311ad2369303ac056277ce0ed243c0913cf
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-09-11 23:04:27 +0300

    Add files via upload

---
+++ Lab_Multidimensional Arrays/Sum Matrix Elements/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArr = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = myArr[0];
+
+            int cols = myArr[1];
+
+
+            int[,] matrix = new int[rows, cols];
+
+            Console.WriteLine(rows);
+
+            Console.WriteLine(cols);
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currentRow = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+
+                    matrix[row, col] = currentRow[col];
+
+
+
+
+                }
+
+
+
+
+            }
+
+            int sum = 0;
+
+            foreach (var item in matrix)
+            {
+                sum += item;
+
+
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
