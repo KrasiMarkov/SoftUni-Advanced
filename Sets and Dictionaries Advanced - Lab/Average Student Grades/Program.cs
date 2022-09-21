7e065a52c302b1556ecb35b4fc4887bb999e8d78
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-09-21 22:34:30 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Lab/Average Student Grades/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string[] info = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string name = info[0];
+
+                decimal grade = decimal.Parse(info[1]);
+
+                if (!students.ContainsKey(name))
+                {
+                    students[name] = new List<decimal>();
+                }
+
+                students[name].Add(grade);
+            }
+
+            foreach (var item in students)
+            {
+                Console.Write($"{item.Key} -> ");
+
+                foreach (var itemm in item.Value)
+                {
+                    Console.Write($"{itemm:F2} ");
+
+                }
+                Console.WriteLine($"(avg: {item.Value.Average():F2})");
+                
+            }
+        }
+    }
+}
