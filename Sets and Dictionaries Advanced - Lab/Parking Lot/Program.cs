2bdd2f36469dcb8bf31287a6e2fdc7d5857e876b
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-09-25 23:41:29 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Lab/Parking Lot/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> mySet = new HashSet<string>();
+
+            bool flag = false;
+
+            while (true)
+            {
+                string[] cars = Console.ReadLine().Split(", ").ToArray();
+
+                if (cars[0] == "END")
+                {
+                    break;
+                }
+
+                else if (cars[0] == "IN")
+                {
+                    string number = cars[1];
+
+                    mySet.Add(number);
+                }
+
+                else if (cars[0] == "OUT")
+                {
+                    string number = cars[1];
+
+                    if (mySet.Contains(number))
+                    {
+                        mySet.Remove(number);
+                    }
+                }
+            }
+
+            if (mySet.Any())
+            {
+                flag = true;
+            }
+
+            if (flag == true)
+            {
+                foreach (var item in mySet)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
+        }
+    }
+}
