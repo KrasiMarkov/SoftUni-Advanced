099306487045429f16a570305bae8f8524ffdf62
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-09-26 23:41:43 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Lab/Record Unique Names/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> myHash = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string name = Console.ReadLine();
+
+                myHash.Add(name);
+
+            }
+
+            foreach (var item in myHash)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
