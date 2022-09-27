d126a0b73a0e46d07b16bdaddbc5355821667873
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-09-27 23:11:33 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Exercise/Count Symbols/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            SortedDictionary<char, int> sortedSymbols = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char symbol = word[i];
+
+                if (!sortedSymbols.ContainsKey(symbol))
+                {
+                    sortedSymbols[symbol] = 0;
+                }
+
+                sortedSymbols[symbol]++;
+
+            }
+
+            foreach (var item in sortedSymbols)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+
+
+            }
+        }
+    }
+}
