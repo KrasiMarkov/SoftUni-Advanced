ed96a33bb123c07d8f110b1cfc4f3c422caccbe0
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-10-03 19:32:33 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Exercise/Periodic Table/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> chemicalElements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split().ToArray();
+
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    chemicalElements.Add(elements[j]);
+
+
+                }
+            }
+
+            foreach (var item in chemicalElements)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
