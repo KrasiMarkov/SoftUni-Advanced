e57e7e27ec345339605d30d17695135af193d961
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-01-16 23:28:54 +0200

    Add files via upload

---
+++ Lab_Streams, Files and Directories/Folder Size/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(".");
+
+            var totalSize = 0m;
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+
+                totalSize += fileInfo.Length;
+            }
+
+            
+
+            using StreamWriter writer = new StreamWriter("output.txt");
+
+            var final = $"{(totalSize / 1024 / 1024):F4} MB";
+
+            writer.Write(final);
+
+            writer.Flush();
+        }
+    }
+}
