8ea6bca16e66d8db78153d9be0059eab6fcf6fde
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-01-17 23:46:06 +0200

    Add files via upload

---
+++ Lab_Streams, Files and Directories/Line Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream stream = new FileStream("TextFileOne.txt", FileMode.OpenOrCreate);
+
+            FileStream otherStream = new FileStream("TextFileTwo.txt", FileMode.OpenOrCreate);
+
+            using StreamReader reader = new StreamReader(stream);
+
+            using StreamWriter writer = new StreamWriter(otherStream);
+
+            int counter = 1;
+
+            while (true)
+            {
+
+                var line = reader.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                writer.WriteLine($"{counter}. {line}");
+
+                writer.Flush();
+
+                counter++;
+            }
+        }
+    }
+}
