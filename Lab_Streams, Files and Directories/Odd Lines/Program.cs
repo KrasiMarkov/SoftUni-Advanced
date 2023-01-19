8b41d55b83b5a56f206449c262dc7322b3013498
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-01-19 23:58:31 +0200

    Add files via upload

---
+++ Lab_Streams, Files and Directories/Odd Lines/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+namespace Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileStream stream = new FileStream("TextOne.txt", FileMode.OpenOrCreate);
+
+            FileStream anotherStream = new FileStream("TextTwo.txt", FileMode.OpenOrCreate);
+
+            using StreamReader reader = new StreamReader(stream);
+
+            using StreamWriter writer = new StreamWriter(anotherStream);
+
+
+            int counter = 0;
+
+            while (true)
+            {
+
+                var result = reader.ReadLine();
+
+                if (result == null)
+                {
+                    break;
+                }
+
+                if (counter % 2 == 1)
+                {
+                    writer.WriteLine(result);
+
+                    writer.Flush();
+
+                }
+
+
+                counter++;
+
+            }
+
+            
+
+
+
+
+
+        }
+    }
+}
