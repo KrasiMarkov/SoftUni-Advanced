dcbe4fe0f50524b2d55ea3bb1eff498071d00f3b
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-01-21 23:52:28 +0200

    Add files via upload

---
+++ Lab_Streams, Files and Directories/Slice a File/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+namespace Slice_a_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream stream = new FileStream("InputPart.txt", FileMode.OpenOrCreate);
+
+            var parts = 4;
+
+            var lenght = (int)Math.Ceiling(stream.Length / (decimal)parts);
+
+            byte[] buffer = new byte[lenght];
+
+
+            for (int i = 0; i < parts; i++)
+            {
+
+                var bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                if (bytesRead < buffer.Length)
+                {
+                    buffer = buffer.Take(bytesRead).ToArray();
+                }
+
+                
+
+                using FileStream output = new FileStream($"Part{i + 1}.txt", FileMode.OpenOrCreate);
+
+                output.Write(buffer, 0, buffer.Length);
+
+                
+
+
+            }
+
+            
+
+
+
+
+
+
+        }
+    }
+}
