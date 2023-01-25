9a01776b5c3c6a7d9e2d461467784d482287ddc2
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-01-25 23:14:13 +0200

    Add files via upload

---
+++ Exercise_Streams, Files and Directories/Copy Binary File/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace Copy_Binary_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream stream = new FileStream("batman.jpg", FileMode.Open);
+
+            using FileStream streamOutput = new FileStream("newBatman.jpg", FileMode.Create);
+
+            byte[] buffer = new byte[4096];
+
+
+            while (true)
+            {
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+               
+
+                streamOutput.Write(buffer);
+            }
+        }
+    }
+}
