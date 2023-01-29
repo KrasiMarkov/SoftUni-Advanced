cf2e98acb4745e5c914f680b6e139716c4b6fdba
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-01-29 23:24:57 +0200

    Add files via upload

---
+++ Exercise_Streams, Files and Directories/Zip and Extract/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO.Compression;
+namespace Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "batman.jpg";
+
+            using ZipArchive zipArchive = ZipFile.Open("../../../arhiv.zip", ZipArchiveMode.Create);
+
+            zipArchive.CreateEntryFromFile(filePath, filePath);
+        }
+    }
+}
