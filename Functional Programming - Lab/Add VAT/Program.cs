de3cc7cb90c426b4784412568f24419b058f97d6
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-02-02 23:39:27 +0200

    Add files via upload

---
+++ Functional Programming - Lab/Add VAT/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> func = /*ParsePrice;*/ p => p += p * 0.2;
+
+           
+
+            double[] price = Console.ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .Select(func)
+                .ToArray();
+
+            foreach (var item in price)
+            {
+                Console.WriteLine($"{item:F2}");
+            }
+
+
+        }
+        //static double ParsePrice(double price)
+        //{
+
+        //    price += price * 0.2;
+
+        //    return price;
+        //}
+            
+                
+             
+           
+
+    }
+
+}
