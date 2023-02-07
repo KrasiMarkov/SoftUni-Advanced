fbf5325a2bbe2eac5ff12efa7010da0eb2ed5fe4
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-02-07 23:32:57 +0200

    Add files via upload

---
+++ Functional Programming - Lab/Sort Even Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] line = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", line));
+
+
+
+
+
+        }
+    }
+}
