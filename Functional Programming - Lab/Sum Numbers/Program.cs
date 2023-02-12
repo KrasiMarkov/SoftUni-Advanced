857b40ac026279f85ec17cf9167440b9affa7b98
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-02-12 23:45:29 +0200

    Add files via upload

---
+++ Functional Programming - Lab/Sum Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] line = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Console.WriteLine(line.Count());
+            Console.WriteLine(line.Sum());
+
+
+        }
+    }
+}
