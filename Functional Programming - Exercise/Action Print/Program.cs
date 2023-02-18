43a72a157c5c6a13113469ce44f1e64d202c351b
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-02-18 16:25:47 +0200

    Add files via upload

---
+++ Functional Programming - Exercise/Action Print/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+namespace Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split().ToArray();
+
+            Action<string[]> myFunc = a => Console.WriteLine(string.Join(Environment.NewLine, a));
+
+            myFunc(line);
+
+        }
+
+       
+    }
+}
