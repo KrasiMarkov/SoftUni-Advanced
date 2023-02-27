8abdfda670e795a3c13d9b699d8c2036007b835c
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-02-27 23:17:15 +0200

    Add files via upload

---
+++ Functional Programming - Exercise/Custom Min Function/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            //Func<int[], int> Myfunc = n => n.Min();
+
+            Func<int[], int> myFunc = minNum;
+
+            int result = myFunc(numbers);
+
+            Console.WriteLine(result);
+
+        }
+
+        static int minNum(int[] numbers)
+        {
+
+            int minNum = int.MaxValue;
+
+            foreach (var item in numbers)
+            {
+                if (item < minNum)
+                {
+                    minNum = item;
+                }
+                
+            }
+
+
+
+            return minNum;
+        }
+    }
+}
