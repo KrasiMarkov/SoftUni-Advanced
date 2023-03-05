52c9094c81c5ccd1be551fcc5f6fe6ce1c18a5bf
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-03-05 23:37:30 +0200

    Add files via upload

---
+++ Knights of Honor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            //names = names.Select(n => $"Sir {n}").ToList();
+
+            names = MySelect(names, n => $"Sir {n}");
+
+            Action<List<string>> myFunc = a => Console.WriteLine(string.Join(Environment.NewLine, a));
+
+            myFunc(names);
+        }
+
+        static List<string> MySelect(List<string> names, Func<string, string> func)
+        {
+            List<string> newList = new List<string>();
+
+            foreach (var item in names)
+            {
+                string newItem = func(item);
+
+                newList.Add(newItem);
+            }
+
+            return newList;
+        }
+    }
+}
