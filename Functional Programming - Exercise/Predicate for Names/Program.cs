4501ff8d86d7d1c6f6fe14bd3bd01fb540201f41
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-03-10 23:30:40 +0200

    Add files via upload

---
+++ Functional Programming - Exercise/Predicate for Names/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Predicate_for_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Func<string, bool> myFunc = x => x.Length <= n;
+
+            names = names.Where(myFunc).ToList();
+
+            names.ForEach(Console.WriteLine);
+
+            
+
+
+        }
+    }
+}
