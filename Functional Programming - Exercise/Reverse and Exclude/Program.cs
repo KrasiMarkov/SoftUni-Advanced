7c02433db59f9aeecfb73f0da06a83c7969d0413
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-03-13 23:04:56 +0200

    Add files via upload

---
+++ Functional Programming - Exercise/Reverse and Exclude/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Reverse_and_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            numbers.Reverse();
+
+            Func<List<int>, int, List<int>> myFunc = RemoveNumbers;
+
+            var result = myFunc(numbers, n);
+
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+
+        static List<int> RemoveNumbers(List<int> numbers, int n)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int current = numbers[i];
+
+                if (current % n == 0)
+                {
+                    numbers.Remove(current);
+                    
+                    i--;
+                }
+
+
+            }
+
+
+            return numbers;
+        }
+    }
+}
